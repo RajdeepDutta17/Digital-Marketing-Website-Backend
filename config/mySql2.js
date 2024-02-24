a77049f22073fde76dea1c65245e5f324e90aabe
RajdeepDutta17 <dutta10rajdeep@gmail.com>
2024-02-24 23:04:24 +0530

    changes made

--- config/mySql2.js
+++ config/mySql2.js
@@ -1,10 +1,11 @@
 const mysql = require("mysql2/promise");
+require("dotenv").config();
 
 const pool = mysql.createPool({
-  host: "localhost",
-  user: "root",
-  database: "form",
-  password: "",
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
   waitForConnections: true,
   connectionLimit: 10,
 });
